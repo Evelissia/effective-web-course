855c76a8c4bf9d2bb3977a0b102670c73ea7b349
Evelissia <evelissia@gmail.com>
2022-11-11 20:07:52 +0600

    Feat: created styles for the timer

---
+++ lab2/timer/script.js
@@ -0,0 +1,12 @@
+const oneMinute = document.getElementById('1min');
+const fiveMinutes = document.getElementById('5min');
+const tenMinute = document.getElementById('10min');
+
+const minutesInput = document.getElementById('minutes');
+const secondsInput = document.getElementById('seconds');
+
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+const audio = new Audio("./audio/finish.mp3");
